89422cf3009d623f1d1c48bc079fecb945a8a705
slaterslater <slaterslater@gmail.com>
2021-01-13 16:00:02 -0500

    add bookChoice()

--- index.js
+++ index.js
@@ -1,27 +1,88 @@
-require('dotenv').config();
-const env = process.env;
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const readline = require('readline');
+const shelf = require('./books.json');
+// const { env } = require('process');
+require('dotenv').config();
 
-(async function oreilly() {
+
+const openOreilly = async (ebook) => {
+  const env = process.env;
   let driver = await new Builder().forBrowser('firefox').build();
-  let chapter = getChapter();
   try{
-    await driver.get(`${env.LOGIN}?user=${env.LIBRARY_CARD}&pass=${env.PIN}&url=${env.PORTAL}`);
-    driver.findElement(By.className('cta')).click();
+    await driver.get(`${env.LOGIN}?user=${env.CARD}&pass=${env.PIN}&url=${env.PORTAL}`);
+    await driver.wait(until.elementLocated(By.className('cta')), 3000).click();
     await driver.wait(until.elementLocated(By.className('successModal')), 3000);
-    await driver.get(env.BOOK + chapter);
+    await driver.get(env.LIBRARY+ebook);
   } catch(err){
     console.log(err);
   }
-})();
-
-function getChapter() {
-  let num = process.argv.slice(2)[0];
-  if (Number(num)){
-    let apter = (num.length == 2) ? num : '0'+num;
-    return `ch${apter}.html`
-  }
-  return '';  
 }
 
-// console.log(getChapter())
+const getChapterUri = (num) =>{
+  return (Number(num)) ? `/ch${(num.length == 2) ? num : '0'+num}.html` : '';
+}
+
+const showBooks = () => {
+  for (book in shelf){
+    console.log(`${parseInt(book) + 1} ${shelf[book].name}`);
+  }
+};
+
+const userChoice = (option) =>{
+  return new Promise((resolve, reject) => {
+    let rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question(option, choice => {
+      rl.close();
+      resolve(choice);
+    });
+  });
+}
+
+const bookChoice = async () => {
+  let ebook, selection, chapter = '';
+  showBooks(); 
+  selection = await userChoice(`enter 1-${shelf.length}: `);
+  ebook = getEbookUri(selection);
+  if (ebook != '/home'){
+    chapter = await userChoice('chapter: ');
+  }
+  ebook += getChapterUri(chapter);
+  console.log(ebook);
+  // openOreilly(ebook);
+};
+
+const argv = (num) => {
+  return process.argv.slice(num)[0];
+}
+
+const getEbookUri = (book) => {
+  let uri = '/home';
+  if ((Number(book)) && book > 0 && book <= shelf.length)
+    uri = `/library/view/${shelf[book-1].name}/${shelf[book-1].isbn}`;
+  return uri;
+}
+
+const start = (async () => {
+  switch(process.argv.length) {
+    case 3:
+      ebook = getEbookUri(argv(2));
+      console.log(ebook);
+      break;
+    case 4:
+      ebook = getEbookUri(argv(2)) + chpt(argv(3));
+      console.log(ebook);
+      break;
+    default:
+      bookChoice();
+  }
+})();  
+
+/* 2Do
+save favourite book
+save current chapter for each book
+
+go to current chapter
+*/
