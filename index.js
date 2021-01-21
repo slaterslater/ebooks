daff5a793773b321293d837892c7e484cc34933b
slaterslater <slaterslater@gmail.com>
2021-01-21 18:30:04 -0500

    format with prettier

--- index.js
+++ index.js
@@ -1,69 +1,73 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const readline = require('readline');
-const shelf = require('./books.json');
-require('dotenv').config();
-
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const readline = require("readline");
+const shelf = require("./books.json");
+require("dotenv").config();
 
 const openOreilly = async (ebook) => {
   const env = process.env;
-  let driver = await new Builder().forBrowser('firefox').build();
-  try{
-    await driver.get(`${env.LOGIN}?user=${env.CARD}&pass=${env.PIN}&url=${env.PORTAL}`);
-    await driver.wait(until.elementLocated(By.className('cta')), 3000).click();
-    await driver.wait(until.elementLocated(By.className('successModal')), 3000);
-    await driver.get(env.LIBRARY+ebook);
-  } catch(err){
+  let driver = await new Builder().forBrowser("firefox").build();
+  try {
+    await driver.get(
+      `${env.LOGIN}?user=${env.CARD}&pass=${env.PIN}&url=${env.PORTAL}`
+    );
+    await driver.wait(until.elementLocated(By.className("cta")), 3000).click();
+    await driver.wait(until.elementLocated(By.className("successModal")), 3000);
+    await driver.get(env.LIBRARY + ebook);
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
-const getChapterUri = (num) =>{
-  return (Number(num)) ? `/ch${(num.length == 2) ? num : '0'+num}.html` : '';
-}
+const getChapterUri = (num) => {
+  return Number(num) ? `/ch${num.length == 2 ? num : "0" + num}.html` : "";
+};
 
 const showBooks = () => {
-  for (book in shelf){
+  for (book in shelf) {
     console.log(`${parseInt(book) + 1} ${shelf[book].name}`);
   }
 };
 
-const userChoice = (option) =>{
+const userChoice = (option) => {
   return new Promise((resolve, reject) => {
     let rl = readline.createInterface({
       input: process.stdin,
-      output: process.stdout
+      output: process.stdout,
     });
-    rl.question(option, choice => {
+    rl.question(option, (choice) => {
       rl.close();
       resolve(choice);
     });
   });
-}
+};
 
 const bookChoice = async () => {
-  let ebook, selection, chapter = '';
-  showBooks(); 
+  let ebook,
+    selection,
+    chapter = "";
+  showBooks();
   selection = await userChoice(`enter 1-${shelf.length}: `);
   ebook = getEbookUri(selection);
-  if (ebook != '/home'){
-    chapter = await userChoice('chapter: ');
+  if (ebook != "/home") {
+    chapter = await userChoice("chapter: ");
   }
   ebook += getChapterUri(chapter);
   return ebook;
 };
 
 const getEbookUri = (book) => {
-  let uri = '/home';
-  if ((Number(book)) && book > 0 && book <= shelf.length){
-    const {name, isbn} = shelf[book-1];
+  let uri = "/home";
+  if (Number(book) && book > 0 && book <= shelf.length) {
+    const { name, isbn } = shelf[book - 1];
     uri = `/library/view/${name}/${isbn}`;
   }
   return uri;
-}
+};
 
 (async () => {
-  let ebook, argv = num => process.argv.slice(num)[0];
-  switch(process.argv.length) {
+  let ebook,
+    argv = (num) => process.argv.slice(num)[0];
+  switch (process.argv.length) {
     case 3:
       ebook = getEbookUri(argv(2));
       break;
@@ -71,10 +75,10 @@ const getEbookUri = (book) => {
       ebook = getEbookUri(argv(2)) + getChapterUri(argv(3));
       break;
     default:
-      ebook = await bookChoice().catch((err)=>{
+      ebook = await bookChoice().catch((err) => {
         console.log(`${err}\nsomething went wrong...`);
       });
   }
   // console.log(ebook);
   openOreilly(ebook);
-})();  
+})();
